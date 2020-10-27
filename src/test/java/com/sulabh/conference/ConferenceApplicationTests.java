1a94b402cd831f57f2d3ccf7251f5a253a07e591
Sulabh Bartaula <sulabhbartaula@gmail.com>
2020-10-28 10:29:05 +1100

    First Commit: Working configured App in JAR Deployment

---
+++ src/test/java/com/sulabh/conference/ConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sulabh.conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
